79d262f771883cdda9ff1f33eba51f8020683be0
eliplutchok <eliplutchok1@gmail.com>
2024-08-14 00:13:44 -0400

    added sorting for ratings and notes

--- controllers/getAllController.js
+++ controllers/getAllController.js
@@ -3,7 +3,7 @@ const logger = require('../config/logger');
 
 exports.allEdits = async (req, res) => {
     logger.info('Query:', req.query);
-    const { book, page_number, status, username, currentPage = 1, limit = 20, fetchAll = false } = req.query;
+    const { book, page_number, status, username, currentPage = 1, limit = 20, fetchAll = false, sortField = 'creation_date', sortOrder = 'asc' } = req.query;
     const offset = (currentPage - 1) * limit;
     const client = await pool.connect();
 
@@ -61,14 +61,30 @@ exports.allEdits = async (req, res) => {
             countQuery += whereClause;
         }
 
+        // Add sorting to the query
+        const validSortFields = ['book_name', 'page_number', 'text', 'creation_date', 'status', 'username', 'notes', 'hebrew_text'];
+        if (validSortFields.includes(sortField)) {
+            if (sortField === 'page_number') {
+                query += `
+                    ORDER BY
+                    CAST(regexp_replace(page_number, '[^0-9]', '', 'g') AS INTEGER) ${sortOrder === 'desc' ? 'DESC' : 'ASC'},
+                    CASE
+                        WHEN page_number ~ 'a$' THEN 0
+                        WHEN page_number ~ 'b$' THEN 1
+                        ELSE 2
+                    END ${sortOrder === 'desc' ? 'DESC' : 'ASC'}
+                `;
+            } else {
+                query += ` ORDER BY ${sortField} ${sortOrder === 'desc' ? 'DESC' : 'ASC'}`;
+            }
+        }
+
         if (!fetchAll) {
             query += ` LIMIT $${queryParams.length + 1} OFFSET $${queryParams.length + 2}`;
             queryParams.push(limit, offset);
         }
 
-
         const result = await client.query(query, queryParams);
-
         const countResult = await client.query(countQuery, countParams);
 
         res.json({ edits: result.rows, totalPages: Math.ceil(countResult.rows[0].total / limit) });
@@ -82,31 +98,31 @@ exports.allEdits = async (req, res) => {
 
 exports.allRatings = async (req, res) => {
     logger.info('Query:', req.query);
-    const { username, translation_status, rating_status, currentPage = 1, limit = 20, fetchAll = false } = req.query;
+    const { username, translation_status, rating_status, currentPage = 1, limit = 20, fetchAll = false, sortField = 'creation_date', sortOrder = 'asc' } = req.query;
     const offset = (currentPage - 1) * limit;
     const client = await pool.connect();
 
     try {
         let query = `
-        SELECT ratings.rating_id, ratings.rating, ratings.feedback, ratings.creation_date, ratings.status,
-               translations.text, translations.status AS translation_status,
-               passages.hebrew_text, passages.passage_id, pages.page_number, books.name AS book_name, users.username
-        FROM ratings
-        JOIN translations ON ratings.translation_id = translations.translation_id
-        JOIN passages ON translations.passage_id = passages.passage_id
-        JOIN pages ON passages.page_id = pages.page_id
-        JOIN books ON pages.book_id = books.book_id
-        JOIN users ON ratings.user_id = users.user_id
-    `;
-    let countQuery = `
-        SELECT COUNT(*) AS total
-        FROM ratings
-        JOIN translations ON ratings.translation_id = translations.translation_id
-        JOIN passages ON translations.passage_id = passages.passage_id
-        JOIN pages ON passages.page_id = pages.page_id
-        JOIN books ON pages.book_id = books.book_id
-        JOIN users ON ratings.user_id = users.user_id
-    `;
+            SELECT ratings.rating_id, ratings.rating, ratings.feedback, ratings.creation_date, ratings.status,
+                   translations.text, translations.status AS translation_status,
+                   passages.hebrew_text, passages.passage_id, pages.page_number, books.name AS book_name, users.username
+            FROM ratings
+            JOIN translations ON ratings.translation_id = translations.translation_id
+            JOIN passages ON translations.passage_id = passages.passage_id
+            JOIN pages ON passages.page_id = pages.page_id
+            JOIN books ON pages.book_id = books.book_id
+            JOIN users ON ratings.user_id = users.user_id
+        `;
+        let countQuery = `
+            SELECT COUNT(*) AS total
+            FROM ratings
+            JOIN translations ON ratings.translation_id = translations.translation_id
+            JOIN passages ON translations.passage_id = passages.passage_id
+            JOIN pages ON passages.page_id = pages.page_id
+            JOIN books ON pages.book_id = books.book_id
+            JOIN users ON ratings.user_id = users.user_id
+        `;
         let queryParams = [];
         let countParams = [];
         let whereClauses = [];
@@ -134,6 +150,12 @@ exports.allRatings = async (req, res) => {
             const whereClause = ` WHERE ` + whereClauses.join(' AND ');
             query += whereClause;
             countQuery += whereClause;
+        }
+
+        // Add sorting to the query
+        const validSortFields = ['book_name', 'page_number', 'text', 'creation_date', 'status', 'username', 'feedback', 'hebrew_text', 'rating'];
+        if (validSortFields.includes(sortField)) {
+            query += ` ORDER BY ${sortField} ${sortOrder === 'desc' ? 'DESC' : 'ASC'}`;
         }
 
         if (!fetchAll) {
